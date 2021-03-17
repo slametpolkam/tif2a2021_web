96933b04bae9ce0c147e0f252a4b2e852310ab30
slametpolkam <slamet@poltek-kampar.ac.id>
2021-03-17 11:09:53 +0700

    Sourc Code TIF 2A 2021

---
+++ natif/index.php
@@ -0,0 +1,25 @@
+<html>
+<head>
+	<title>latihan HTML</title>
+</head>
+<BODY>
+	<h1>FORM LOGIN</h1><BR>
+	<FORM ACTION="proses.php" METHOD="POST">
+		<B>Nama Pengguna </B>
+		<INPUT TYPE="TEXT" name="nama" PLACEHOLDER="Nama">
+		<br><br>
+		<B>Sandi Pengguna </B>
+		<INPUT type="password" name="sandi" placeholder="Sandi Disini">
+		<br>
+		<input type="submit" value="Pencet Daku">
+	</FORM>
+</BODY>
+</html>
+<?php	
+	// for($i=0;$i<10;$i++){
+	// 	echo "<center>";
+	// 	for($z=0;$z<$i;$z++){
+	// 		echo "* ";
+	// 	}
+	// 	echo "<br></center>";
+	// }
---
+++ natif/proses.php
@@ -0,0 +1,5 @@
+<?php
+	$namaandaadalah=$_POST["nama"];
+	$sandi=$_POST["sandi"];
+
+	echo "<h1>Nama Anda Adalah ".$namaandaadalah."&nbsp dan sandi anda adalah ".$sandi."</h1>";
